eacbdf852bae1c330997dbab2ebdb64b61f8d7df
piraat-arrr <85346814+piraat-arrr@users.noreply.github.com>
2021-06-04 15:42:13 +0200

    Update sources.js

--- sources.js
+++ sources.js
@@ -1,3 +1,11 @@
+var alerts = [
+    {
+        "type":"warning",
+        "title":"Isn't this illegal?",
+        "content":"It's not! We do not host the series ourselves so we do not violate DMCA policies. We simply index existing sources. Have some fun!"
+    }
+];
+
 var sources = [];
 
 var raw_sources = ``;
